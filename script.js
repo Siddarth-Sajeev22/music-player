ff71e28bfa7fb581110579f73440190cca156c6e
Neorace22 <siddarthsajeev2002@gmail.com>
2023-04-02 01:03:08 +0530

    fixed minor errors
    Fixed error with icons not changing and improved overall functioning

--- script.js
+++ script.js
@@ -2,36 +2,41 @@ console.log("Music Player");
 
 let songIndex=0;
 let audioElement = new Audio('./songs/NuvoleBianche.mp3');
-let masterPlay = document.getElementById('masterplay');
-let myProgressBar = document.getElementById('myprogressbar');
-let gif = document.getElementById('gif');
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
 let songs = [
     {songname: "Nuvole Bianche", filepath:"songs/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
-    {songname: "Nuvole Bianche", filepath:"song/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
-    {songname: "Nuvole Bianche", filepath:"song/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
-    {songname: "Nuvole Bianche", filepath:"song/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
-    {songname: "Nuvole Bianche", filepath:"song/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
+    {songname: "Aankhon Mein Teri Ajab Si", filepath:"songs\Aankhon Mein Teri Ajab Si.mp3", coverpath: "cover/NuvoleBianche.png"},
+    {songname: "Nuvole Bianche", filepath:"songs/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
+    {songname: "Nuvole Bianche", filepath:"songs/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
+    {songname: "Nuvole Bianche", filepath:"songs/NuvoleBianche.mp3", coverpath: "cover/NuvoleBianche.png"},
 ]
 
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0)
     {
         audioElement.play();
-        masterPlay.classList.remove('fa-solid fa-play fa-lg');
-        masterPlay.classList.add('fa-solid fa-pause');
+        masterPlay.classList.remove('fa-solid', 'fa-play', 'fa-lg');
+        masterPlay.classList.add('fa-solid', 'fa-pause', 'fa-lg');
         gif.style.opacity = 1;
     }
     else 
     {
         audioElement.pause();
-        masterPlay.classList.remove('fa-solid fa-pause');
-        masterPlay.classList.add('fa-solid fa-play fa-lg');
+        masterPlay.classList.remove('fa-solid', 'fa-pause', 'fa-lg');
+        masterPlay.classList.add('fa-solid', 'fa-play', 'fa-lg');
         gif.style.opacity = 0;
-        
     }
 })
 
-myProgressBar.addEventListener('timeupdate',()=>{
+audioElement.addEventListener('timeupdate',()=>{
     console.log('timeupdate');
-}
-)
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = (audioElement.duration * myProgressBar.value)/100;
+})
